a967608c59044ce300d4e73b0bdebb0023fc468f
ommina <lloyd@shaw.ca>
2019-07-28 18:41:11 -0600

    Don't render internal faces of glass blocks. Give glass blocks the same "no mob spawning" and "no suffocating" rules as vanilla glass blocks.

--- src/main/java/net/ommina/wallpapercraft/blocks/DecorativeBlockGlass.java
+++ src/main/java/net/ommina/wallpapercraft/blocks/DecorativeBlockGlass.java
@@ -1,8 +1,15 @@
 package net.ommina.wallpapercraft.blocks;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.EntityType;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockReader;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
 import javax.annotation.Nonnull;
 
@@ -16,6 +23,33 @@ public class DecorativeBlockGlass extends DecorativeBlockPatterned {
     @Override
     public BlockRenderLayer getRenderLayer() {
         return BlockRenderLayer.TRANSLUCENT;
+    }
+
+    @Override
+    public boolean propagatesSkylightDown( BlockState state, IBlockReader reader, BlockPos pos ) {
+        return true;
+    }
+
+    @Override
+    public boolean causesSuffocation( BlockState state, IBlockReader worldIn, BlockPos pos ) {
+        return false;
+    }
+
+    @Override
+    public boolean canEntitySpawn( BlockState state, IBlockReader worldIn, BlockPos pos, EntityType<?> type ) {
+        return false;
+    }
+
+    @Override
+    public boolean isNormalCube( BlockState state, IBlockReader worldIn, BlockPos pos ) {
+        return false;
+    }
+
+    @OnlyIn( Dist.CLIENT )
+    @Override
+    public boolean isSideInvisible( BlockState state, BlockState adjacentBlockState, Direction side ) {
+
+        return adjacentBlockState.getBlock() == this || super.isSideInvisible( state, adjacentBlockState, side );
     }
 
 }
